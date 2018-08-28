0ca01b97c311f01bc6fa94258882b84dc4264a6b
Maksim Litvinov <1058433+rxx@users.noreply.github.com>
2018-08-28 18:50:26 +0300

    Generate database stuff by soda generator

---
+++ 6-database/models/task.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/uuid"
+	"github.com/gobuffalo/validate"
+	"github.com/gobuffalo/validate/validators"
+)
+
+type Task struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Title     string    `json:"title" db:"title"`
+	When      string    `json:"when" db:"when"`
+	Status    string    `json:"status" db:"status"`
+}
+
+// String is not required by pop and may be deleted
+func (t Task) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Tasks is not required by pop and may be deleted
+type Tasks []Task
+
+// String is not required by pop and may be deleted
+func (t Tasks) String() string {
+	jt, _ := json.Marshal(t)
+	return string(jt)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (t *Task) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: t.Title, Name: "Title"},
+		&validators.StringIsPresent{Field: t.When, Name: "When"},
+		&validators.StringIsPresent{Field: t.Status, Name: "Status"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (t *Task) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (t *Task) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
